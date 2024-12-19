ce16e2db68adca37a7a56055d13ae0b61ddc0336
Dreycod <84332780+Dreycod@users.noreply.github.com>
2024-12-19 16:25:36 +0100

    test

--- SaintMichel/obj/Debug/net9.0-ios/iossimulator-x64/SaintMichel.AssemblyInfo.cs
+++ SaintMichel/obj/Debug/net9.0-ios/iossimulator-x64/SaintMichel.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SaintMichel")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+62b64de93ed7a3bd389c27c3081df8d72d3c191a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+5dfa28be6fb6e7ad1c86b0adbd839000089e3b74")]
 [assembly: System.Reflection.AssemblyProductAttribute("SaintMichel")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SaintMichel")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
